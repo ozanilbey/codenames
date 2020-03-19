8abfb7a9611452376a311705074749c24b5c586f
Ozan İlbey Yılmaz <ozanilbey@ozanilbey.net>
2020-03-19 21:02:50 +0300

    Updates

---
+++ precache-manifest.0a63ced4536eeab36932f8418366def2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7b27da0a0413a312e2a38d3237bacf29",
+    "url": "/codenames/index.html"
+  },
+  {
+    "revision": "390eeb3ad5621dcb118b",
+    "url": "/codenames/static/css/main.c70da2d0.chunk.css"
+  },
+  {
+    "revision": "df453e4073dd24a95bd6",
+    "url": "/codenames/static/js/2.f91ffec1.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/codenames/static/js/2.f91ffec1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "390eeb3ad5621dcb118b",
+    "url": "/codenames/static/js/main.475cce4a.chunk.js"
+  },
+  {
+    "revision": "9579fd12880f171e4e10",
+    "url": "/codenames/static/js/runtime-main.5ea03339.js"
+  }
+]);
